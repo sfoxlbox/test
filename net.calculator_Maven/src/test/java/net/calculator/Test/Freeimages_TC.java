eb4f4c9cdf8dae672f80b95f422c55af89d45b67
sfoxlbox <mounir.sounni@gmail.com>
2022-08-09 02:06:43 -0400

    commit

---
+++ net.calculator_Maven/src/test/java/net/calculator/Test/Freeimages_TC.java
@@ -0,0 +1,34 @@
+package net.calculator.Test;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import net.calculator.base;
+import net.calculator.po.freeimages_po;
+
+public class Freeimages_TC extends base{
+WebDriver driver;
+	
+	@BeforeMethod
+	public void Setup()
+	{
+		driver = setproperty();
+		driver.get("https://calculator.net/");
+	}
+	@Test
+	public void Test_location() throws InterruptedException 
+	{
+		freeimages_po Fitness = new freeimages_po(driver);
+		
+		Fitness.links();
+	}
+	  @AfterMethod
+	   public  void TearDown()
+	   { 
+		driver.close();
+		driver.quit();
+		
+	}
+}
