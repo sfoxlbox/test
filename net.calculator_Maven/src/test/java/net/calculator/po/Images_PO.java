b6fdea4f8b5fc23b0c6982d0f92570a0b07d4315
sfoxlbox <mounir.sounni@gmail.com>
2022-08-10 10:45:05 -0400

    commt

--- net.calculator_Maven/src/test/java/net/calculator/po/Images_PO.java
+++ net.calculator_Maven/src/test/java/net/calculator/po/Images_PO.java
@@ -34,10 +34,11 @@ WebDriver driver;
 		WebElement link = driver.findElement(By.xpath("//table/tbody/tr/td[\"+x+\"]/div[1]/a"));
 		
 		WebElement logo = driver.findElement(By.tagName("img"));
-		//Thread.sleep(1000);
+
 		WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(20));
 		Actions action = new Actions(driver);
 		SoftAssert sa = new SoftAssert();
+		
 		
 		action.sendKeys(Keys.F5);
 		String text;
